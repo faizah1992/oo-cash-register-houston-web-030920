31cac30028e0ea5795b53d44530b74f67ed2e927
FAIZAH AHSAN <faizah1992@gmail.com>
2020-03-12 14:57:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+class CashRegister
+  
+  attr_accessor :discount, :total
+  
+  def initialize(discount = 0)
+    @total= 0
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity = 1)
+    price *= quantity
+    self.total += price
+  end 
 
+  def apply_discount
+    self.total -
+  
+end
